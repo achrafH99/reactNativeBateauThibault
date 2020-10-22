db924ac6d5f7f05d170304fde9ea56281936a03b
ayoub <ayoub.bhija77@gmail.com>
2020-10-22 17:32:31 +0200

    add Header to categories

--- screens/CategoryList.js
+++ screens/CategoryList.js
@@ -4,10 +4,15 @@ import Img from '../constants/image'
 import categorie from '../constants/categorie'
 import List from '../components/List';
 import { SafeAreaView } from 'react-native';
+import { Header } from 'react-native-elements';
 
 export default function CategoryList({ navigation }) {
     return (
         <SafeAreaView>
+                <Header
+                    containerStyle = {{backgroundColor: "#008cdc",height : 100, paddingBottom:0, marginBottom: 0}}
+                    centerComponent={{ text: 'Categories', style: { color: '#fff', fontWeight:"bold", fontSize:32 }}}
+                />
                     <List navigation={navigation} list={categorie.map(({ name, screen, id }) => {
             return <Category key={name} navigation={navigation} name={name} image={Img.poulpe.uri} screen={screen} id={id} />
         })}/>
